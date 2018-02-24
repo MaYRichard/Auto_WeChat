3e3560b2a1871060729067fcfec10984c809ef83
MaYRichard <crystalyc@163.com>
2018-02-24 22:54:38 +0800

    完成消息接受和语音播放

--- Read_WeChat.py
+++ Read_WeChat.py
@@ -2,19 +2,39 @@ from win32com.client import constants
 import pyttsx3
 import win32com.client
 import pythoncom
-import itchat
+import itchat, time
+from itchat.content import *
 
-@itchat.msg_register(itchat.content.TEXT)
-def print_content(msg):
-    text = (msg['Text'])
+engine = pyttsx3.init()
+
+@itchat.msg_register(TEXT)
+def _(msg):
+    # equals to print(msg['FromUserName'])
+    print(msg.fromUserName)
+
+
+@itchat.msg_register([TEXT, MAP, CARD, NOTE, SHARING])
+def text_reply(msg):
+    text = '好友%s说: %s' % (msg.User.RemarkName, msg.text)
     print(text)
-    engine = pyttsx3.init()
     engine.say(text)
-    engine.setProperty('volume', 1)
+    engine.setProperty('volume', 0.9)
     engine.runAndWait()
+    #msg.user.send('%s: %s' % (msg.type, msg.text))
+
+@itchat.msg_register([PICTURE, RECORDING, ATTACHMENT, VIDEO])
+def download_files(msg):
+    msg.download(msg.fileName)
+    typeSymbol = {
+        PICTURE: 'img',
+        VIDEO: 'vid', }.get(msg.type, 'fil')
+    return '@%s@%s' % (typeSymbol, msg.fileName)
+
+@itchat.msg_register(FRIENDS)
+def add_friend(msg):
+    msg.user.verify()
+    msg.user.send('Nice to meet you!')
 
 
-if __name__ == '__main__':
-    itchat.auto_login(enableCmdQR=True, hotReload=True)
-    itchat.run()
-
+itchat.auto_login(True)
+itchat.run(True)
